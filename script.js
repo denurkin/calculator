499f81626a8d98e518b77692cd011f9b0e4d7aa6
Denis Iurkin <denurkin@gmail.com>
2021-07-30 00:54:34 +0300

    NEW

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const a =1;
-let b =2;
-let c;
-let param = 23;
+let money = '1';
+const income = '2';
+let addExpenses = '3';
+const deposit = '4';
+let mission = '5';
+let period = '6';
+allert ('привет');
+console.log ('привет');
