11ab2eace6ac90dc6a72a1f2c7d4b9e4ceaf7171
Denis Iurkin <denurkin@gmail.com>
2021-07-30 17:27:24 +0300

    NEW

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-let money = '1';
-const income = '2';
-let addExpenses = '3';
-const deposit = '4';
-let mission = '5';
-let period = '6';
+let money = 100000;
+console.log (typeof money);
+const income = 'фриланс';
+console.log (typeof income);
+let addExpenses = 'Курсы Отпуск Проездной';
+console.log (addExpenses.toLowerCase());
+console.log (addExpenses.length);
+console.log (addExpenses.split(" "));
+let deposit = true;
+console.log (typeof deposit);
+let mission = 500000;
+let period = 8;
+console.log (period === mission);
+let budgetDay  = money / 30;
+console.log (budgetDay);
 alert ("привет, мир");
 console.log ('привет');
+
