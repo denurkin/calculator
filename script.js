9ec5f81957945a5b95e9b0e1543261d3639625fb
Denis Iurkin <denurkin@gmail.com>
2021-08-02 17:21:34 +0300

    New

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+'use strict';
 let money = 100000;
 console.log (typeof money);
 const income = 'фриланс';
@@ -16,4 +17,37 @@ let budgetDay  = money / 30;
 console.log (budgetDay);
 alert ("привет, мир");
 console.log ('привет');
-
+// дз 3 день
+money = prompt('Ваш месячный доход?');
+money;
+console.log(money);
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+addExpenses;
+console.log(addExpenses);
+deposit = confirm('Есть ли у вас депозит в банке?');
+deposit;
+console.log(deposit);
+let expenses1 = prompt('Введите обязательную статью расходов?');
+expenses1;
+console.log(expenses1);
+let expenses2 = prompt('Введите обязательную статью расходов?');
+expenses2;
+console.log(expenses2);
+let amount1 = prompt('Во сколько это обойдется?');
+amount1;
+console.log(amount1);
+let amount2 = prompt('Во сколько это обойдется?');
+amount2;
+console.log(amount2);
+let budgetMonth = money - amount1 - amount2;
+console.log (budgetMonth);
+console.log (Math.ceil(mission / budgetMonth));
+if (budgetDay > 1200){
+console.log ("У вас высокий уровень дохода");
+} else if (budgetDay<=1200 && budgetDay>600){
+console.log ("У вас средний уровень дохода");
+} else if (budgetDay<=600 && budgetDay>0){
+console.log ("К сожалению у вас уровень дохода ниже среднего");
+} else {
+    console.log ("Что то пошло не так");
+    } ;
